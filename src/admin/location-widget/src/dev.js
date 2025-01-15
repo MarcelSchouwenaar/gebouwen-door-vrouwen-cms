9da22aac9f0a8b29663392ac3d8451cf42ded64d
Marcel Sch <marcelschouwenaar@gmail.com>
2025-01-15 09:43:53 +0100

    cleaned up repo, removed custom widget

--- src/admin/location-widget/src/dev.js
+++
@@ -1,4 +0,0 @@
-// Codes below will not be included when running in "production" mode.
-window.CMS_MANUAL_INIT = true;
-CMS.registerWidget('test', window.StarterControl, window.StarterPreview);
-CMS.init();
